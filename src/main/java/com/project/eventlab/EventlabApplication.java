3133a06441b9cd64c8e8b436c377c05933600eb7
eduardo-toste <duciochetti@gmail.com>
2026-05-23 19:00:59 -0300

    feat: create initial project

---
+++ src/main/java/com/project/eventlab/EventlabApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/eventlab/EventlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
