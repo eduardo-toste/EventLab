88d656cf4564a54e8b956da87008c68cc415ded8
eduardo-toste <duciochetti@gmail.com>
2026-05-29 20:11:12 -0300

    feat: create event envelope to use as a default event structure

---
+++ src/main/java/com/project/eventlab/event/EventEnvelope.java
@@ -0,0 +1,14 @@
+package com.project.eventlab.event;
+
+import java.time.LocalDateTime;
+
+public record EventEnvelope<T>(
+
+        String eventId,
+        String eventType,
+        String version,
+        LocalDateTime occurredAt,
+        T data
+
+) {
+}
