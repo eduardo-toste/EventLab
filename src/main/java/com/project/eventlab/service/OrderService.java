cff958220852baa9bc9c67b2a700a86260b11c2f
eduardo-toste <duciochetti@gmail.com>
2026-05-29 20:11:25 -0300

    feat: create order service class

---
+++ src/main/java/com/project/eventlab/service/OrderService.java
@@ -0,0 +1,48 @@
+package com.project.eventlab.service;
+
+import com.project.eventlab.dto.OrderCreatedData;
+import com.project.eventlab.dto.OrderRequest;
+import com.project.eventlab.dto.OrderResponse;
+import com.project.eventlab.event.EventEnvelope;
+import com.project.eventlab.producer.OrderProducer;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderProducer orderProducer;
+
+    public OrderService(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    public OrderResponse createOrder(OrderRequest request) {
+        String orderId = UUID.randomUUID().toString();
+
+        OrderCreatedData data = new OrderCreatedData(
+                orderId,
+                request.customerId(),
+                request.total()
+        );
+
+        EventEnvelope<OrderCreatedData> event = new EventEnvelope<>(
+                UUID.randomUUID().toString(),
+                "ORDER_CREATED",
+                "1",
+                LocalDateTime.now(),
+                data
+        );
+
+        orderProducer.publishOrderCreated(orderId, event);
+
+        return new OrderResponse(
+                orderId,
+                "CREATED",
+                "Order created event published"
+        );
+    }
+
+}
