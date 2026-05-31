c881acf8690e47567974ac785e12c0d055dc58b4
eduardo-toste <duciochetti@gmail.com>
2026-05-31 16:10:57 -0300

    feat: add validations on order resquest dto

--- src/main/java/com/project/eventlab/dto/OrderRequest.java
+++ src/main/java/com/project/eventlab/dto/OrderRequest.java
@@ -1,10 +1,16 @@
 package com.project.eventlab.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record OrderRequest(
 
+        @NotBlank(message = "Customer id must not be blank")
         String customerId,
+
+        @NotNull(message = "Total id must not be null")
         BigDecimal total
 
 ) {
