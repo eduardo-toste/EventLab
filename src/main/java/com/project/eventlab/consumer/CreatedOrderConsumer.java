b57011d459d526240986b6447d227200d1e3ea8e
eduardo-toste <duciochetti@gmail.com>
2026-06-02 18:40:26 -0300

    refactor: update consumer logger method

--- src/main/java/com/project/eventlab/consumer/CreatedOrderConsumer.java
+++ src/main/java/com/project/eventlab/consumer/CreatedOrderConsumer.java
@@ -1,19 +1,38 @@
 package com.project.eventlab.consumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.eventlab.dto.OrderCreatedData;
 import com.project.eventlab.event.EventEnvelope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreatedOrderConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(CreatedOrderConsumer.class);
+
+    private final ObjectMapper objectMapper;
+
+    public CreatedOrderConsumer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @KafkaListener(
             topics = "${app.kafka.topics.order-created}",
             groupId = "order-created-logger"
     )
-    public void consume(EventEnvelope<OrderCreatedData> event) {
-        System.out.println("Event: " + event);
+    public void consume(EventEnvelope<?> event) {
+        OrderCreatedData data = objectMapper.convertValue(event.data(), OrderCreatedData.class);
+
+        log.info(
+                "[ORDER_CREATED_CONSUMED] eventId={} correlationId={} orderId={} total={}",
+                event.eventId(),
+                event.correlationId(),
+                data.orderId(),
+                data.total()
+        );
     }
 
 }
