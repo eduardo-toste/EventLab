ecb46cd93d631b177fcd3b55662bf2735a7692fa
eduardo-toste <duciochetti@gmail.com>
2026-06-09 21:34:07 -0300

    feat: create payment processed event to send to notification service

--- src/main/java/com/project/eventlab/consumer/PaymentProcessedConsumer.java
+++ src/main/java/com/project/eventlab/consumer/PaymentProcessedConsumer.java
@@ -1,8 +1,10 @@
 package com.project.eventlab.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.eventlab.dto.order.OrderCreatedData;
 import com.project.eventlab.dto.payment.PaymentProcessedData;
 import com.project.eventlab.event.EventEnvelope;
+import com.project.eventlab.service.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,9 +16,11 @@ public class PaymentProcessedConsumer {
     private static final Logger log = LoggerFactory.getLogger(PaymentProcessedConsumer.class);
 
     private final ObjectMapper objectMapper;
+    private final NotificationService notificationService;
 
-    public PaymentProcessedConsumer(ObjectMapper objectMapper) {
+    public PaymentProcessedConsumer(ObjectMapper objectMapper, NotificationService notificationService) {
         this.objectMapper = objectMapper;
+        this.notificationService = notificationService;
     }
 
     @KafkaListener(
@@ -26,6 +30,15 @@ public class PaymentProcessedConsumer {
     public void consume(EventEnvelope<?> event) {
         PaymentProcessedData data = objectMapper.convertValue(event.data(), PaymentProcessedData.class);
 
+        EventEnvelope<PaymentProcessedData> paymentProcessedEvent = new EventEnvelope<>(
+                event.eventId(),
+                event.correlationId(),
+                event.eventType(),
+                event.version(),
+                event.occurredAt(),
+                data
+        );
+
         log.info(
                 "[PAYMENT_PROCESSED_CONSUMED] eventId={} correlationId={} orderId={} paymentId={} total={}",
                 event.eventId(),
@@ -34,6 +47,8 @@ public class PaymentProcessedConsumer {
                 data.paymentId(),
                 data.amount()
         );
+
+        notificationService.sendNotification(paymentProcessedEvent);
     }
 
 }
